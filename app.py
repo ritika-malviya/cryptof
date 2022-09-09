c1ec8d76c25044e9403b515a20575dc11ad39854
Ritika Malviya <103213849+ritika-malviya@users.noreply.github.com>
2022-09-09 20:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ end_date = d1
 d2 = date.today() - timedelta(days=1825)  # 5 years data
 d2 = d2.strftime("%Y-%m-%d")
 start_date = d2
-st.title("Cryptocurrencies Prediction using LSTM")
+st.title("Cryptocurrencies Prediction using LSTM for next 1 month")
 st.image(image)
 st.subheader("What is Cryptocurrency???")
 st.write("""
